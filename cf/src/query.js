a4db0d15b45e5a92ad5164f17f67bfe14b8af178
Max Kuklin <8312264+max-kuklin@users.noreply.github.com>
2025-09-03 21:48:59 +0100

    Revert "Add query timeout option to cancel long-running queries"

--- cf/src/query.js
+++ cf/src/query.js
@@ -23,15 +23,13 @@ export class Query extends Promise {
     this.state = null
     this.statement = null
 
-    this.resolve = x => (this.active = false, this.clearTimeouts(), resolve(x))
-    this.reject = x => (this.active = false, this.clearTimeouts(), reject(x))
+    this.resolve = x => (this.active = false, resolve(x))
+    this.reject = x => (this.active = false, reject(x))
 
     this.active = false
     this.cancelled = null
     this.executed = false
     this.signature = ''
-    this.timeoutTimer = null
-    this.timedOut = false
 
     this[originError] = this.handler.debug
       ? new Error()
@@ -52,28 +50,7 @@ export class Query extends Promise {
   }
 
   cancel() {
-    this.clearTimeouts()
     return this.canceller && (this.canceller(this), this.canceller = null)
-  }
-
-  clearTimeouts() {
-    if (this.timeoutTimer) {
-      clearTimeout(this.timeoutTimer)
-      this.timeoutTimer = null
-    }
-  }
-
-  setTimeout() {
-    const postgresOptions = this.options.postgres_options
-    if (postgresOptions && postgresOptions.query_timeout && !this.timeoutTimer) {
-      this.timeoutTimer = setTimeout(() => {
-        if (this.active && !this.cancelled) {
-          // Mark as cancelled to distinguish from manual cancellation
-          this.timedOut = true
-          this.cancel()
-        }
-      }, postgresOptions.query_timeout * 1000)
-    }
   }
 
   simple() {
@@ -160,7 +137,7 @@ export class Query extends Promise {
   }
 
   async handle() {
-    !this.executed && (this.executed = true) && await 1 && (this.setTimeout(), this.handler(this))
+    !this.executed && (this.executed = true) && await 1 && this.handler(this)
   }
 
   execute() {
